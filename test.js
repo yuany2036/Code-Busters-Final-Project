be14a313ee1e435d72be745fda5ec4ab79ccce5b
Yu-An <yuany2036@gmail.com>
2023-04-03 15:17:45 +0200

    update

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ console.log("Hello Best team ever!");
 
 // sorry :P I had to do it
 
-console.log("No need to be sorry, its the plain truth!!")
+console.log("No need to be sorry, its the plain truth!!");
 
 console.log("sorry I had to do it");
 
@@ -12,6 +12,10 @@ function test() {
 
 function itsOkay() {
   console.log("its okay! all shall be forgiven");
+}
+
+function maybe() {
+  console.log("maybe not");
 }
 
 test();
