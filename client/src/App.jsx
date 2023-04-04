acd3869ca88a807c403a4f0e9139f9e1940fa8f0
Yu-An <yuany2036@gmail.com>
2023-04-04 11:57:57 +0200

    setting up context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useContext } from "react";
+import DataContext from "./data/context";
+// import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <h1>Best Team Ever's Project!</h1>
       <h1>first commit test</h1>
+      {/* <HomePage></HomePage> */}
     </div>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { useContext } from "react";
+import DataContextProvider from "./data/context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DataContextProvider>
+      <App />
+    </DataContextProvider>
+  </React.StrictMode>
+);
