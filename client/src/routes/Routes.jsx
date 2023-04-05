db6a87e8232a9a568f511e776f4287b634305222
Yu-An <yuany2036@gmail.com>
2023-04-05 15:48:04 +0200

    adding route

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../components/homePage/HomePage';
 import ExplorePage from '../components/explorePage/ExplorePage';
+import Animation from '../components/landingPage/booksAnimation/Animation';
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/explore" element={<ExplorePage />} />
+    <Route path="/animation" element={<Animation />} />
   </Routes>
 );
 
