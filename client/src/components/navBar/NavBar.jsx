28a3152a05f4d8ba85130f41b81c42f64def134e
GladysAW <gladys.aldoumani@hotmail.com>
2023-04-17 11:51:17 +0200

    first commit to feature branch

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,31 +1,67 @@
-import React from "react";
-import classes from "./NavBar.module.scss";
-import Menu from "~icons/mdi/menu";
+import { useState } from 'react';
+import styles from './NavBar.module.scss';
+import Menu from '~icons/mdi/menu';
+import Close from '~icons/mdi/close';
+import { NavLink } from 'react-router-dom';
+import { NavBarButtons, NavBarData } from './NavBarData';
 
 const NavBar = () => {
-  return (
-    <div>
-      <div className={classes.navbar}>
-        <div className={classes.logo}>
-          <a href="#">LOGO</a>
-        </div>
-        <ul className={classes.links}>
-          <li>
-            <a href="#">Explore</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-        </ul>
+  const [dropDownMenu, setDropDownMenu] = useState(false);
 
-        <a href="#" className={classes.action_btn}>
-          Login
-        </a>
-        <div className={classes.toggle_btn}>
-          <Menu />
+  const showDropDownMenu = () => {
+    setDropDownMenu(!dropDownMenu);
+  };
+
+  const navBarData = NavBarData.map((item, index) => {
+    return (
+      <li key={index}>
+        <NavLink
+          to={item.path}
+          className={styles.navbar_links}
+          target={item.target}
+        >
+          {item.title}
+        </NavLink>
+      </li>
+    );
+  });
+
+  const navBarButtons = NavBarButtons.map((item, index) => {
+    return (
+      <NavLink key={index} to={item.path} className={styles.btn_links}>
+        <button>{item.title}</button>
+      </NavLink>
+    );
+  });
+
+  return (
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.navbar_left}>
+          <div className={styles.logo}>LOGO</div>
+          <ul className={styles.links_left}>{navBarData}</ul>
+        </div>
+        <div className={styles.btns_container_navbar}>{navBarButtons}</div>
+        <div className={styles.toggle_btn}>
+          {!dropDownMenu && (
+            <Menu className={styles.menuBarsIcon} onClick={showDropDownMenu} />
+          )}
+          {dropDownMenu && (
+            <Close className={styles.closeIcon} onClick={showDropDownMenu} />
+          )}
         </div>
       </div>
-    </div>
+      <div
+        className={
+          dropDownMenu
+            ? `${styles.dropdown_menu} ${styles.open}`
+            : `${styles.dropdown_menu}`
+        }
+      >
+        {navBarData}
+        <div className={styles.btns_container_dropdown}>{navBarButtons}</div>
+      </div>
+    </>
   );
 };
 
---
+++ client/src/components/navBar/NavBarData.jsx
@@ -0,0 +1,25 @@
+export const NavBarData = [
+  {
+    title: 'Explore',
+    path: '/explore',
+    target: '_self',
+  },
+  {
+    title: 'About',
+    path: '/about',
+    target: '_self',
+  },
+];
+
+export const NavBarButtons = [
+  {
+    title: 'Login',
+    path: '/login',
+    target: '_blank',
+  },
+  {
+    title: 'Register',
+    path: '/register',
+    target: '_blank',
+  },
+];
