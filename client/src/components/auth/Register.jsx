c841db9aee0df2b71421d628fc2a602b1886ef3b
Mihaela <strechii.mihaela@gmail.com>
2023-04-19 22:24:08 +0200

    adding the styling to register and login pages

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -4,6 +4,8 @@ import { Navigate, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { signup } from '../../apiCalls/userApiCalls';
 import styles from '../auth/Register.module.scss';
+import img from '../../assets/Mobile login-pana.png';
+import { Icon } from '@iconify/react';
 
 const Register = () => {
   const { usersDispatch, isUserLoggedIn } = useContext(DataContext);
@@ -34,86 +36,101 @@ const Register = () => {
 
   return (
     <div className={styles.register_container}>
-      <div className="form-container">
+      <div className={styles.img_container}>
+        <img src={img} alt="" />
+        <a href="https://storyset.com/phone">Illustrations by Storyset</a>
+      </div>
+      <div className={styles.register}>
+        <h1>Register. Connect. Discover. </h1>
+        <h2>EntScape.</h2>
+        <p>
+          Join us and unlock the full potential of our tools for an enhanced
+          movie and book lovers experience.
+        </p>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="info">
-            <div className="row">
-              <div>
-                <input
-                  name="username"
-                  placeholder="Username"
-                  {...register('username', {
-                    required: 'Please put your first name.',
-                  })}
-                />
-                <div className="error-message">
-                  {errors.firstName && <span>{errors.firstName.message}</span>}
-                </div>
-              </div>
-              <div>
-                <input
-                  name="firstName"
-                  placeholder="First name"
-                  {...register('firstName', {
-                    required: 'Please put your first name.',
-                  })}
-                />
-                <div className="error-message">
-                  {errors.firstName && <span>{errors.firstName.message}</span>}
-                </div>
-              </div>
-              <div>
-                <input
-                  name="lastName"
-                  placeholder="Last name"
-                  {...register('lastName', {
-                    required: 'Please put your last name.',
-                  })}
-                />
-                <div className="error-message">
-                  {errors.lastName && <span>{errors.lastName.message}</span>}
-                </div>
-              </div>
-            </div>
-
-            <input
-              name="email"
-              placeholder="Email"
-              {...register('email', {
-                required: 'Please put your email sir.',
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'Email is invalid. Please fix',
-                },
-              })}
-            />
-            <div className="error-message">
-              {errors.email && <span>{errors.email.message}</span>}
-            </div>
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              {...register('password', {
-                required: 'Required',
-                minLength: {
-                  value: 5,
-                  message: 'Password must be at least 5 characters',
-                },
-              })}
-            />
-            <div className="error-message">
-              {errors.password && <span>{errors.password.message}</span>}
-            </div>
+          <input
+            name="username"
+            placeholder="Username"
+            {...register('username', {
+              required: 'Please put your first name.',
+            })}
+          />
+          <div className={styles.error_message}>
+            {errors.firstName && <span>{errors.firstName.message}</span>}
           </div>
 
-          <div className="submit">
-            <input className="button-bg" type="submit" value="Sign Up" />
+          <input
+            name="firstName"
+            placeholder="First name"
+            {...register('firstName', {
+              required: 'Please put your first name.',
+            })}
+          />
+          <div className={styles.error_message}>
+            {errors.firstName && <span>{errors.firstName.message}</span>}
+          </div>
+
+          <input
+            name="lastName"
+            placeholder="Last name"
+            {...register('lastName', {
+              required: 'Please put your last name.',
+            })}
+          />
+          <div className={styles.error_message}>
+            {errors.lastName && <span>{errors.lastName.message}</span>}
+          </div>
+
+          <input
+            name="email"
+            placeholder="Email"
+            {...register('email', {
+              required: 'Enter your password',
+              minLength: {
+                value: 8,
+                message: 'Password must be at least 8 characters',
+              },
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email is invalid. Please fix',
+              },
+            })}
+          />
+          <div className={styles.error_message}>
+            {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            {...register('password', {
+              required: 'Required',
+              minLength: {
+                value: 5,
+                message: 'Password must be at least 5 characters',
+              },
+            })}
+          />
+          <div className={styles.error_message}>
+            {errors.password && <span>{errors.password.message}</span>}
+          </div>
+
+          <div className={styles.submit}>
+            <input type="submit" value="Sign Up" />
           </div>
         </form>
-        <div className="right">
-          <button>Connect with Facebook</button>
-          <button>Connect with Google</button>
+        <div className={styles.connect}>
+          <p>
+            <span>-</span> or register with <span>-</span>
+          </p>
+          <div className={styles.icons}>
+            <a>
+              <Icon icon="logos:facebook" color="#7e57c2" />
+            </a>
+            <a>
+              <Icon icon="devicon:google" color="#7e57c2" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
