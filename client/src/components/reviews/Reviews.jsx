b1546e16e2d414745d303888e2881e75642d945c
GladysAW <gladys.aldoumani@hotmail.com>
2023-04-20 15:54:34 +0200

    commiting

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -38,47 +38,45 @@ const Reviews = ({ reviews }) => {
   return (
     <div className={styles.reviews_container}>
       <h3 className={styles.heading}>Reviews:</h3>
-      {reviews
-        // slice(0, 3).
-        .map(
-          (
-            {
-              author,
-              author_details: { rating, username },
-              content,
-              created_at,
-              // updated_at,
-              id,
-            },
-            index
-          ) => {
-            console.log(index);
-            const date = new Date(created_at);
-            const formattedDate = date.toLocaleDateString();
-            return (
-              <div
-                key={index}
-                // className={styles.review_container}
-              >
-                <h5>{`${
-                  author ? author : username
-                } reviewed on ${formattedDate}`}</h5>
-                <h6>{creatingReviewStar(rating)}</h6>
-                <p>
-                  {expandedReviews.includes(id)
-                    ? content
-                    : `${content.slice(0, 200)}...`}
-                  <button
-                    className={styles.buttons}
-                    onClick={readMoreHandler.bind(id)}
-                  >
-                    {expandedReviews.includes(id) ? '(hide)' : '(read more)'}
-                  </button>
-                </p>
-              </div>
-            );
-          }
-        )}
+      {reviews.slice(0, 3).map(
+        (
+          {
+            author,
+            author_details: { rating, username },
+            content,
+            created_at,
+            // updated_at,
+            id,
+          },
+          index
+        ) => {
+          console.log(index);
+          const date = new Date(created_at);
+          const formattedDate = date.toLocaleDateString();
+          return (
+            <div
+              key={index}
+              // className={styles.review_container}
+            >
+              <h5>{`${
+                author ? author : username
+              } reviewed on ${formattedDate}`}</h5>
+              <h6>{creatingReviewStar(rating)}</h6>
+              <p>
+                {expandedReviews.includes(id)
+                  ? content
+                  : `${content.slice(0, 200)}...`}
+                <button
+                  className={styles.buttons}
+                  onClick={readMoreHandler.bind(id)}
+                >
+                  {expandedReviews.includes(id) ? '(hide)' : '(read more)'}
+                </button>
+              </p>
+            </div>
+          );
+        }
+      )}
       <button className={styles.buttons}>Read all reviews</button>
     </div>
   );
