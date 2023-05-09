1e9b078e4dd531e9d38026aecf576727126b53ee
Yu-An <yuany2036@gmail.com>
2023-05-09 11:44:57 +0200

    changing port

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const port = process.env.PORT || '4000';
+const port = process.env.PORT || '10000';
 
 app.listen(port, console.log(`Server is running on ${port}`));
