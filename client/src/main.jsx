fa8305d0ffbf3912c78c3706c942160e63fd38cd
Yu-An <yuany2036@gmail.com>
2023-05-09 15:48:57 +0200

    changing route method

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { DataContextProvider } from './data/context';
 import { ToastContainer } from 'react-toastify';
@@ -8,9 +8,9 @@ import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <DataContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     <ToastContainer position="top-left" />
   </DataContextProvider>
 );
