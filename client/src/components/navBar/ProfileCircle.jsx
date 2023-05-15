5fc94224cb3d3bfab1aefb7dcf459a00b2f761e5
GladysAW <gladys.aldoumani@hotmail.com>
2023-05-15 09:32:09 +0200

    reverting some changes

--- client/src/components/navBar/ProfileCircle.jsx
+++ client/src/components/navBar/ProfileCircle.jsx
@@ -3,7 +3,7 @@ import styles from './ProfileCircle.module.scss';
 import DataContext from '../../data/context';
 import { useNavigate } from 'react-router-dom';
 
-function ProfileCircle({ showDropDownMenu }) {
+function ProfileCircle(showDropDownMenu) {
   const { user } = useContext(DataContext);
   const [userName, setUserName] = useState('');
   const [avatarUrl, setAvatarUrl] = useState('');
@@ -31,7 +31,11 @@ function ProfileCircle({ showDropDownMenu }) {
   return (
     <div onClick={goToProfile} className={styles.circle}>
       {avatarUrl ? (
-        <img src={avatarUrl} alt="Profile" />
+        <img
+          src={avatarUrl}
+          alt="Profile"
+          style={{ width: '100%', height: '100%', borderRadius: '30px' }}
+        />
       ) : (
         userName && userName.charAt(0).toUpperCase()
       )}
